502711b180760dc09f54b33b2d6b6c99e201e448
Joos Tristan <tristan.joos@student.howest.be>
2026-04-30 09:55:25 +0200

    makes the MovieEventDataFilter InTimeRange

--- src/Howestprime.Movies.Application/Contracts/Data/Filters/MovieEventDataFilters.cs
+++ src/Howestprime.Movies.Application/Contracts/Data/Filters/MovieEventDataFilters.cs
@@ -1,3 +1,6 @@
+using System.Linq.Expressions;
+using Howestprime.Movies.Application.Contracts.Data;
+
 public static class MovieEventDataFilters
 {
     public static Expression<Func<MovieEventData, bool>> InTimeRange(DateTime fromDate, DateTime toDate)
